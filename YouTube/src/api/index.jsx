fa3100b95719964b4646ae3bbe6f97a2900383a5
MD7709 <malvaniyadhaval2004@gmail.com>
2024-07-03 10:49:49 +0530

    first commit

--- YouTube/src/api/index.jsx
+++ YouTube/src/api/index.jsx
@@ -1,7 +1,7 @@
 // api/index.js
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5500/' });
+const API = axios.create({ baseURL: 'http://localhost:4000/' });
 
 API.interceptors.request.use(req => {
   if (localStorage.getItem('Profile')) {
